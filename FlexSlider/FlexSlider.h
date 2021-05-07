31048d1de9fd3178c7bd9ebd347c1127e77c8fb3
Martin Jacob Rehder <gitrepocon01@rehsco.com>
2021-05-07 13:40:38 +0200

    Initial Commit

---
+++ FlexSlider/FlexSlider.h
@@ -0,0 +1,18 @@
+//
+//  FlexSlider.h
+//  FlexSlider
+//
+//  Created by Martin Rehder on 07.05.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FlexSlider.
+FOUNDATION_EXPORT double FlexSliderVersionNumber;
+
+//! Project version string for FlexSlider.
+FOUNDATION_EXPORT const unsigned char FlexSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FlexSlider/PublicHeader.h>
+
+
